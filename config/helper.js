06fc7cd4af49add7931b897e24c6b123f09dc65c
张露 <2424733678@qq.com>
2018-05-30 17:48:40 +0800

    Add  config/help.js  config/pool.js

---
+++ config/helper.js
@@ -0,0 +1,8 @@
+export async function  tryCatch(promise) {
+    try {
+        const ret = await promise
+        return [ret, null]
+    } catch (e) {
+        return [null, e]
+    }
+}
---
+++ config/pool.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql')
+export const pool  = mysql.createPool({
+    connectionLimit : 100,
+    host            : 'localhost',
+    user            : 'nwpu',
+    password        : 'nwpuorder',
+    database        : 'Nwpu2015303320'
+})
+
+export function query(connection, queryStr, inputs) {
+    return new Promise(function (resolve, reject) {
+        connection.query(queryStr, inputs, function (error, results) {
+        if (error) {
+            reject(error)
+        } else {
+            resolve(results)
+        }
+        })
+    })
+}
