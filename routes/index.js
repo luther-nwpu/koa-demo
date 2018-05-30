999a35f442e99daa13d15f4da54767705bbb9b37
张露 <2424733678@qq.com>
2018-05-30 21:40:23 +0800

    Update index.js 添加测试用例

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,22 @@
 const router = require('koa-router')()
+const db = require('../config/pool')
+const helper = require('../config/helper')
 
-router.get('/', async (ctx, next) => {
-
+router.get('/test', async (ctx, next) => {
+  const res = await new Promise(function (resolve, reject) {
+    db.pool.getConnection(async (error, connection) => {
+      if (error) {
+        reject(error)
+      }
+      const [res, queryerror] = helper.tryCatch(await db.query(connection, `select * from testdata`, []))
+      if (queryerror) {
+        reject(queryerror)
+      }
+      resolve(res)
+      connection.release()
+    })
+  })
+  ctx.body = res
 })
 
 router.get('/string', async (ctx, next) => {
