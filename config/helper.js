e38fda18eaea5936bebf3383393990b99ae16e19
张露 <2424733678@qq.com>
2018-06-04 22:09:02 +0800

    Update helper.js 添加获取用户秘密信息注释信息

--- config/helper.js
+++ config/helper.js
@@ -34,10 +34,10 @@ const createError = (errMessage, status, userMessage) => {
 }
 
 /**
- *
- * @param { String } code 获取用户的
- * @param { String } appid
- * @param { String } secret
+ * 获取openid 和 session_key
+ * @param { String } code 用户登录凭证（有效期五分钟）。开发者需要在开发者服务器后台调用 api，使用 code 换取 openid 和 session_key 等信息
+ * @param { String } appid 微信小程序appid
+ * @param { String } secret 微信小程序的密钥appkey
  */
 const privateData = async (code, appid, secret) => {
     // https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code
@@ -54,28 +54,28 @@ const privateData = async (code, appid, secret) => {
     })
 }
 
+/**
+ * 返回用户的敏感信息
+ * @param {*} encryptedData 包括敏感数据在内的完整用户信息的加密数据
+ * @param {*} sessionKey 会话密钥
+ * @param {*} iv 加密算法的初始向量，详细见加密数据解密算法
+ * @param {*} appId 微信小程序的appId
+ */
 const decryptData = (encryptedData, sessionKey, iv, appId) => {
     // base64 decode
     sessionKey = Buffer.from(sessionKey, 'base64')
-
     encryptedData = Buffer.from(encryptedData, 'base64')
-
     iv = Buffer.from(iv, 'base64')
-
     try {
         // 解密
-
         let decipher = crypto.createDecipheriv('aes-128-cbc', sessionKey, iv)
         // 设置自动 padding 为 true，删除填充补位
         decipher.setAutoPadding(true)
-
         /* eslint-disable */
         var decoded = decipher.update(encryptedData, 'binary', 'utf8')
-       
         /* eslint-enable */
 
         decoded += decipher.final('utf8')
-
         decoded = JSON.parse(decoded)
     } catch (err) {
         throw err
