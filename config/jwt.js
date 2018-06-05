0b0ed391d0f9b855928b8170a76cb3208353a7f3
张露 <2424733678@qq.com>
2018-06-05 18:54:52 +0800

    Update jwt.js 设置token有效时间为30Fenzhogn1

--- config/jwt.js
+++ config/jwt.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken')
 const secret = 'Npu2015303320'
-const tokenMaxage = '3d' // 保存 3 天
+const tokenMaxage = '0.5h' // 保存 30 分钟
 
 const productToken = (userinfo) => {
     return jwt.sign({data: userinfo}, secret, { expiresIn: tokenMaxage })
