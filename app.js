8b7baeb15d3cc64f3d7b15d385ce451ff1cbb349
张露 <2424733678@qq.com>
2018-06-05 18:54:10 +0800

    Update app.js 设置koa-jwt 取消login

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(async (ctx, next) => {
     console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
-app.use(koaJwt({'secret': secret}).unless({ path: [/^\/login/, /^\/users/] }))
+app.use(koaJwt({'secret': secret}).unless({ path: [/^\/users/] }))
 
 // routes
 app.use(index.routes(), index.allowedMethods())
