c7f65bc6e9d6d5790a07b201a52541a3a36a286c
张露 <2424733678@qq.com>
2018-06-05 14:30:10 +0800

    fix:bug Error重新命名

--- routes/users.js
+++ routes/users.js
@@ -26,32 +26,32 @@ const getUserinfo = async (code, appId, appKey, encryptedData, iv) => {
 
 router.post('/', async (ctx, next) => {
     const req = ctx.request.body
-    const [res, error] = helper.tryCatch(await new Promise(async(resolve, reject) => {
-        const [userres, error0] = helper.tryCatch(getUserinfo(req.code, req.appId, req.appKey, req.encryptedData, req.iv))
-        if (error) { reject(error0) }
+    const [res, error0] = helper.tryCatch(await new Promise(async(resolve, reject) => {
+        const [userres, error1] = helper.tryCatch(getUserinfo(req.code, req.appId, req.appKey, req.encryptedData, req.iv))
+        if (error1) { reject(error1) }
 
-        db.pool.getConnection(async (error, connection) => {
-            if (error) { reject(error) }
+        db.pool.getConnection(async (error2, connection) => {
+            if (error2) { reject(error2) }
             let userId
-            const [existRes, error1] = helper.tryCatch(await db.query(connection, `select user_id from users where wx_openid = ?`, [userres.openid]))
-            if (error1) { reject(error1) }
+            const [existRes, error3] = helper.tryCatch(await db.query(connection, `select user_id from users where wx_openid = ?`, [userres.openid]))
+            if (error3) { reject(error3) }
             if (existRes.length === 1) {
                 userId = existRes[0]
-                const [, error2] = helper.tryCatch(await db.query(connection, `update users set wx_info = ? where user_id = ?`, [userres.wx_info, userId]))
-                if (error2) { reject(error2) }
+                const [, error4] = helper.tryCatch(await db.query(connection, `update users set wx_info = ? where user_id = ?`, [userres.wx_info, userId]))
+                if (error4) { reject(error4) }
             } else {
                 userId = uuid.v1()
-                const [, error3] = helper.tryCatch(await db.query(connection, `insert into users(user_id, wx_openid, wx_info) values(?, ?, ?)`, [userId, userres.openid, userres.wx_info]))
-                if (error3) { reject(error3) }
+                const [, error5] = helper.tryCatch(await db.query(connection, `insert into users(user_id, wx_openid, wx_info) values(?, ?, ?)`, [userId, userres.openid, userres.wx_info]))
+                if (error5) { reject(error5) }
             }
-            const [queryRes, error4] = helper.tryCatch(await db.query(connection, `select user_id, wx_openid, wx_info, stuid, createTime, updateTime from users where user_id = ?`, [userId]))
-            if (error4) { reject(error4) }
+            const [queryRes, error6] = helper.tryCatch(await db.query(connection, `select user_id, wx_openid, wx_info, stuid, createTime, updateTime from users where user_id = ?`, [userId]))
+            if (error6) { reject(error6) }
             const token = jwt.productToken(userId)
             resolve({ token: token, sessionToken: queryRes })
             connection.release()
         })
     }))
-    if (error) { next(error) }
+    if (error0) { next(error0) }
     ctx.body = res
 })
 
